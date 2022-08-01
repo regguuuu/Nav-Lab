e9f8bbb9d685c4a9d1491d3b33b4fd83ce1babad
regguuuu <Reggie.Leon@cdpipelinedevshop.com>
2022-08-01 15:58:07 +0000

    nav lab

---
+++ nav-lab/src/Components/SidebarData.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import MailIcon from '@mui/icons-material/Mail';
+import Analytics from '@mui/icons-material/Analytics';
+import Dashboard from '@mui/icons-material/Dashboard';
+import GroupIcon from '@mui/icons-material/Group';
+import InfoIcon from '@mui/icons-material/Info';
+
+export const SidebarData = [
+    {
+        title: "Home",
+        icon: <HomeIcon />,
+        link: '/home'
+    },
+    {
+        title: "Mail",
+        icon: <MailIcon />,
+        link: '/mail'
+    },
+    {
+        title: "Analytics",
+        icon: <Analytics />,
+        link: '/analytics'
+    },
+    {
+        title: "Dashboard",
+        icon: <Dashboard />,
+        link: '/Dashboard'
+    },
+    {
+        title: "Group",
+        icon: <GroupIcon />,
+        link: '/groupicon'
+    },
+    {
+        title: "Info",
+        icon: <InfoIcon />,
+        link: '/infoicon'
+    },
+    
+
+
+
+
+]
